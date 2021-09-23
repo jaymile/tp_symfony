c39cdf5bbd2b59064b0b1d98fe73155814b51315
aminou aminou sam djemil <djemil.aminou@gmail.com>
2021-09-23 15:39:56 +0200

    page statique ok

---
+++ src/Controller/ReglementInterieurController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReglementInterieurController extends AbstractController
+{
+    /**
+     * @Route("/reglement", name="reglement")
+     */
+    #[Route('/reglement/interieur', name: 'reglement_interieur')]
+    public function index(): Response
+    {
+        return $this->render('reglement_interieur/reglement.html.twig', [
+            'titre' => 'Reglement_Interieur',
+        ]);
+    }
+}
