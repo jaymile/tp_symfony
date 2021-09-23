dac3790ba38d51e4fd77de59ee6a324325933d74
aminou aminou sam djemil <djemil.aminou@gmail.com>
2021-09-23 14:43:47 +0200

    acceuil fait

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="Home")
+     */
+    public function AfficherHome(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'titre' => 'Acceuil'
+        ]);
+    }
+}
