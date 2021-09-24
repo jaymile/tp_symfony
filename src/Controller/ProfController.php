776536f6d35b65c812dd98fee9e329aa2d4a1185
aminou aminou sam djemil <djemil.aminou@gmail.com>
2021-09-24 02:02:01 +0200

    la findall prof est en cour probleme qui semblerai lié au faite que ca soit un Many to many

--- src/Controller/ProfController.php
+++ src/Controller/ProfController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Prof;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfController extends AbstractController
 {
-    #[Route('/prof', name: 'prof')]
+    /**
+     * @Route("/profs", name="profs")
+     */
     public function index(): Response
     {
-        return $this->render('prof/index.html.twig', [
-            'controller_name' => 'ProfController',
+        $repository = $this->getDoctrine()->getRepository(Prof::class); //repository est une varriable qui me sert a sotoker mes deux methodes getDoc et getRep son nom est pas important il pourrai s'appeler toto on senfout
+        $profs = $repository->findAll();
+
+
+        return $this->render('prof/profs.html.twig', [
+            'profs' => $profs,
         ]);
     }
 }
